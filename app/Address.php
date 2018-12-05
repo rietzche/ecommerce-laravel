40362758e37a459725f34fd205e1f018fbd34b0f
rietzche <haritzrahman98@gmail.com>
2018-12-05 14:55:48 +0700

    model address

--- app/Address.php
+++ app/Address.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Address extends Model
 {
-    //
+    protected $fillable = [
+        'id_user',
+        'receiver_name',
+        'number_tlp',
+        'zip_code',
+        'province',
+        'city',
+        'region',
+        'others',
+    ];
 }
